4f84d504038be411c377d8d5a3a6ff56d9efd897
Oleksandra Rudnyk <kamyanskaya@gmail.com>
2022-01-16 09:25:29 +0200

    hw03 done

--- model/index.js
+++ model/index.js
@@ -1,16 +1,5 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
-
-const listContacts = require("./listContacts");
-const getContactById = require("./getContactById");
-const removeContact = require("./removeContact");
-const addContact = require("./addContact");
-const updateContact = require("./updateContact");
+const { Contact } = require("./contacts");
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+  Contact,
 };
